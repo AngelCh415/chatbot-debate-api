77faf35704592ade3f05d28fe9cdbfc32f21b697
Angel Uriel Chávez Morones <pertempus@MacBook-Pro-de-Angel.local>
2025-08-19 14:12:57 -0600

    feat: Edit conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,11 @@
 import pytest
 from fastapi.testclient import TestClient
 
-from app.main import app
+from app.main import app, store
 
 
 @pytest.fixture(scope="function")
 def client() -> TestClient:
     """Fixture to create a test client for FastAPI."""
+    store._data.clear()
     return TestClient(app)
